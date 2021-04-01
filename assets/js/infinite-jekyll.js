9bbe5595575ff560d91e5bb03c072b084de56f7d
amaynez <78620790+amaynez@users.noreply.github.com>
2021-03-31 22:34:23 -0600

    debugging logs added

--- assets/js/infinite-jekyll.js
+++ assets/js/infinite-jekyll.js
@@ -31,6 +31,7 @@ $(function() {
     // If we've scrolled past the loadNewPostsThreshold, fetch posts
     if ((documentHeight - loadNewPostsThreshold) < bottomScrollPosition) {
       fetchPosts();
+      console.log(windowHeight, windowScrollPosition, bottomScrollPosition, documentHeight);
     }
   });
   
