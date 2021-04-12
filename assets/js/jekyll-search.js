04f0a32bdb73ac8951585f385b7c05432b1e09d5
amaynez <78620790+amaynez@users.noreply.github.com>
2021-04-11 22:13:11 -0500

    moved relativebase declaration to outside of the function to make it global.

--- assets/js/jekyll-search.js
+++ assets/js/jekyll-search.js
@@ -1,10 +1,10 @@
+let relativebase = "./"
+
 (function e (t, n, r) { function s (o, u) { if (!n[o]) { if (!t[o]) { var a = typeof require === 'function' && require; if (!u && a) return a(o, !0); if (i) return i(o, !0); throw new Error("Cannot find module '" + o + "'") } var f = n[o] = {exports: {}}; t[o][0].call(f.exports, function (e) { var n = t[o][1][e]; return s(n || e) }, f, f.exports, e, t, n, r) } return n[o].exports } var i = typeof require === 'function' && require; for (var o = 0; o < r.length; o++)s(r[o]); return s })({1: [function (require, module, exports) {
   'use strict'
   module.exports = {
     load: load
   }
-
-  let relativebase = "./"
 
   function load (location, callback) {
     var xhr = getXHR()
