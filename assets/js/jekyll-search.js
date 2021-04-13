ef8c1dea8d888a46eb52110e6c7487831082790a
amaynez <78620790+amaynez@users.noreply.github.com>
2021-04-13 04:45:13 -0500

    made dynamically added scripts to load sequentially.

--- assets/js/jekyll-search.js
+++ assets/js/jekyll-search.js
@@ -1,13 +1,10 @@
 var relativebase = "./";
-console.log("inside", relativebase);
 
 (function e (t, n, r) { function s (o, u) { if (!n[o]) { if (!t[o]) { var a = typeof require === 'function' && require; if (!u && a) return a(o, !0); if (i) return i(o, !0); throw new Error("Cannot find module '" + o + "'") } var f = n[o] = {exports: {}}; t[o][0].call(f.exports, function (e) { var n = t[o][1][e]; return s(n || e) }, f, f.exports, e, t, n, r) } return n[o].exports } var i = typeof require === 'function' && require; for (var o = 0; o < r.length; o++)s(r[o]); return s })({1: [function (require, module, exports) {
   'use strict'
   module.exports = {
     load: load
   }
-
-  console.log("inside e");
 
   function load (location, callback) {
     var xhr = getXHR()
@@ -262,7 +259,7 @@ console.log("inside", relativebase);
 
   /*
     Public API
-  */  console.log("about to declare SimpleJekyllSearch");
+  */
       window.SimpleJekyllSearch = function SimpleJekyllSearch (_options) {
         var errors = optionsValidator.validate(_options)
         if (errors.length > 0) {
