f5c5c2a4dec28afbb83b68ed553aac4751e0f409
amaynez <78620790+amaynez@users.noreply.github.com>
2021-04-16 22:04:41 -0500

    https://github.com/the-mvm/the-mvm.github.io/issues/13

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,7 @@ $(function () {
 
   // Click outside of menu to close it
   flexContainer.click(function (e) {
-    if (flexContainer.hasClass('active') && e.target.tagName !== 'A' && e.target.tagName !== 'label') {
+    if (flexContainer.hasClass('active') && e.target.tagName !== 'A' && !(e.target.className.includes('toggle'))) {
       flexContainer.removeClass('active')
     }
   })
